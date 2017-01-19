1d6b6598ffc75383223746b8b499568b539e6ed7
TheCodeCrafter <noahcoetsee@gmail.com>
2017-01-19 10:55:07 -0500

    Added more sem-colons
    Having to do all the hard work for ye, eh?

--- public/js/RemotePlayer.js
+++ public/js/RemotePlayer.js
@@ -1,43 +1,43 @@
 /* global game */
 
 var RemotePlayer = function (index, game, player, startX, startY, startAngle) {
-  var x = startX
-  var y = startY
-  var angle = startAngle
+  var x = startX;
+  var y = startY;
+  var angle = startAngle;
 
-  this.game = game
-  this.health = 3
-  this.player = player
-  this.alive = true
+  this.game = game;
+  this.health = 3;
+  this.player = player;
+  this.alive = true;
 
-  this.player = game.add.sprite(x, y, 'enemy')
+  this.player = game.add.sprite(x, y, 'enemy');
 
-  this.player.animations.add('move', [0, 1, 2, 3, 4, 5, 6, 7], 20, true)
-  this.player.animations.add('stop', [3], 20, true)
+  this.player.animations.add('move', [0, 1, 2, 3, 4, 5, 6, 7], 20, true);
+  this.player.animations.add('stop', [3], 20, true);
 
-  this.player.anchor.setTo(0.5, 0.5)
+  this.player.anchor.setTo(0.5, 0.5);
 
-  this.player.name = index.toString()
-  game.physics.enable(this.player, Phaser.Physics.ARCADE)
-  this.player.body.immovable = true
-  this.player.body.collideWorldBounds = true
+  this.player.name = index.toString();
+  game.physics.enable(this.player, Phaser.Physics.ARCADE);
+  this.player.body.immovable = true;
+  this.player.body.collideWorldBounds = true;
 
-  this.player.angle = angle
+  this.player.angle = angle;
 
-  this.lastPosition = { x: x, y: y, angle: angle }
+  this.lastPosition = { x: x, y: y, angle: angle };
 }
 
 RemotePlayer.prototype.update = function () {
   if (this.player.x !== this.lastPosition.x || this.player.y !== this.lastPosition.y || this.player.angle != this.lastPosition.angle) {
-    this.player.play('move')
-    this.player.rotation = Math.PI + game.physics.arcade.angleToXY(this.player, this.lastPosition.x, this.lastPosition.y)
+    this.player.play('move');
+    this.player.rotation = Math.PI + game.physics.arcade.angleToXY(this.player, this.lastPosition.x, this.lastPosition.y);
   } else {
-    this.player.play('stop')
+    this.player.play('stop');
   }
 
-  this.lastPosition.x = this.player.x
-  this.lastPosition.y = this.player.y
-  this.lastPosition.angle = this.player.angle
+  this.lastPosition.x = this.player.x;
+  this.lastPosition.y = this.player.y;
+  this.lastPosition.angle = this.player.angle;
 }
 
-window.RemotePlayer = RemotePlayer
+window.RemotePlayer = RemotePlayer;
